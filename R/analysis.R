defb37c5ce47cf7d2da3f2380b8e671df23fc4c3
Isaac Griffith <isaacgriffith@gmail.com>
2016-04-09 12:44:13 -0600

    Added initial analysis script

---
+++ R/analysis.R
@@ -0,0 +1,24 @@
+library(daewr)
+
+# Read in data
+data <- 
+
+# Construct model
+model <- lm(MI ~ Patter*Rot, data=data)
+
+# Verify Assumptions
+
+
+# Conduct Anova
+Anova(model, type="III")
+
+# Interactions
+
+
+# Contrasts
+
+
+# Multiple Comparison
+
+
+# Power Analysis
